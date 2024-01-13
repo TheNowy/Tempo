251e99012dcf33e926151282a4ea9d5079b0da53
NowyTeam <jaj2232ajn@gmail.com>
2024-01-13 09:20:49 +0600

    Update

--- src/components/pages/home/MainTitle/MainTitle.tsx
+++ src/components/pages/home/MainTitle/MainTitle.tsx
@@ -5,26 +5,19 @@ const MainTitle = () => {
     <>
       <div className={Style.Title_Wrapper}>
         <div className={Style.main_title}>
-          <h1>
-            Стань <span>водителем</span> прямо сейчас и начни{" "}
-            <span>зарабатывать</span>
-          </h1>
+          <h1>Стань водителем Yandex Такси и начни зарабатывать</h1>
           {/* <p>
             <span>MangoKG</span> — волшебный мир свободы и заработка! Стань
             таксистом сегодня и ощути ветер перемен вместе с нами!.
           </p> */}
           <div className={Style.Btn_Wrapper}>
-            <button className={Style.Discord_Add}>
-              Стать водителем
-            </button>
+            <button className={Style.Discord_Add}>Подключится</button>
             {/* <button className={Style.App_But}>Подробнее</button> */}
           </div>
         </div>
         <div className={Style.benefits_fast}>
           <div className={Style.benefits_box}>
-            <div className={Style.benef_box}>
-              
-            </div>
+            <div className={Style.benef_box}></div>
           </div>
         </div>
       </div>
