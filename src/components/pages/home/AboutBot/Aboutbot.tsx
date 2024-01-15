d1e22bf486a0cdfef735436feb0170dcfbb32a91
NowyTeam <jaj2232ajn@gmail.com>
2024-01-15 11:12:41 +0600

    Slider

--- src/components/pages/home/AboutBot/Aboutbot.tsx
+++ src/components/pages/home/AboutBot/Aboutbot.tsx
@@ -1,12 +1,34 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import Style from "./Aboutbot.module.scss";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Aboutbot = () => {
   return (
     <>
-      <div className={Style.About_wrapper}>
-        <div className={Style.Info}>
-          <h1>Скоро...</h1>
-        </div>
+      <div className={Style.Slider_Wrapper}>
+        <Swiper
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          // navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className={Style.mySwiper}>
+          <SwiperSlide className={Style.Slider}>Slide 1</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 2</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 3</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 4</SwiperSlide>
+        </Swiper>
       </div>
     </>
   );
