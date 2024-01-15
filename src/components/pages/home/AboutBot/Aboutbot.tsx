e579fcd8b74e07deaabb1c6b13cc2654af734846
NowyTeam <jaj2232ajn@gmail.com>
2024-01-15 15:09:01 +0600

    Adaptation

--- src/components/pages/home/AboutBot/Aboutbot.tsx
+++ src/components/pages/home/AboutBot/Aboutbot.tsx
@@ -12,8 +12,12 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 const Aboutbot = () => {
   return (
     <>
+      {/* PC Media */}
       <div className={Style.Slider_Wrapper}>
         <Swiper
+          spaceBetween={10}
+          slidesPerView={3}
+          loop={true}
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
@@ -23,7 +27,51 @@ const Aboutbot = () => {
           }}
           // navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
-          className={Style.mySwiper}>
+          className={`${Style.mySwiper} custom-swiper-pagination`}>
+          <SwiperSlide className={Style.Slider}>Slide 1</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 2</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 3</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 4</SwiperSlide>
+        </Swiper>
+      </div>
+      {/* Laptop Media */}
+      <div className={Style.Slider_Wrapper_laptop}>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={2}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          // navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className={`${Style.mySwiper} custom-swiper-pagination`}>
+          <SwiperSlide className={Style.Slider}>Slide 1</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 2</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 3</SwiperSlide>
+          <SwiperSlide className={Style.Slider}>Slide 4</SwiperSlide>
+        </Swiper>
+      </div>
+      {/* Phone Media */}
+      <div className={Style.Slider_Wrapper_phone}>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          // navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className={`${Style.mySwiper} custom-swiper-pagination`}>
           <SwiperSlide className={Style.Slider}>Slide 1</SwiperSlide>
           <SwiperSlide className={Style.Slider}>Slide 2</SwiperSlide>
           <SwiperSlide className={Style.Slider}>Slide 3</SwiperSlide>
