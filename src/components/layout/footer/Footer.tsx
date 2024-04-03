499f710a162e6e47d639065310c458029c7a6516
NowyTeam <jaj2232ajn@gmail.com>
2024-04-03 10:01:48 +0600

    Update

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,11 +1,14 @@
-import scss from "./Footer.module.scss";
+import Style from "./Footer.module.scss";
 
 export const Footer = () => {
   return (
     <div>
       <footer>
-        <div className={scss.footer_cont}>
+        <div className={Style.footer_cont}>
           <h1>© 2024 Mango - KG</h1>
+          <h1 className={Style.Designed}>
+            Designed by <span>Nowy</span>
+          </h1>
         </div>
       </footer>
     </div>
